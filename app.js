799e5c1e28f7ba59d220de08cc8a8297a89edd8b
Stefan Carter <stefan.carter@icloud.com>
2022-03-04 09:08:58 +0000

    Fretboard project

---
+++ app.js
@@ -0,0 +1,73 @@
+const root = document.documentElement;
+
+const fretboard = document.querySelector('.fretboard');
+const numberOfFrets = 18;
+const numberOfStrings = 6;
+
+// up to step 10, 11 next 
+
+const singleFretMarkPositions = [3, 5, 7, 9, 15, 17, 19, 21];
+const doubleFretMarkPositions = [12, 24];
+
+const notesFlat = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"];
+const notesSharp = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+let accidentals = 'flats';
+const guitarTuning = [4, 11, 7, 2, 9, 4];
+
+const app = {
+    init() {
+        this.setupFretboard();
+    },
+    setupFretboard() {
+        root.style.setProperty('--number-of-strings', numberOfStrings);
+        // add strings to fretboard
+        for (let i = 0; i < numberOfStrings; i++) {
+            let string = tools.createElement('div');
+            string.classList.add('string');
+            fretboard.appendChild(string);
+
+            // create frets
+            for (let fret = 0; fret <= numberOfFrets; fret++) {
+                let noteFret = tools.createElement('div');
+                noteFret.classList.add('note-fret');
+                string.appendChild(noteFret);
+
+                let noteName = this.generateNoteNames((fret + guitarTuning[i]), accidentals);
+                noteFret.setAttribute('data-note', noteName);
+
+                // add single fretmarks
+                if (i === 0 && singleFretMarkPositions.indexOf(fret) !== -1) {
+                    noteFret.classList.add('single-fretmark');
+                }
+
+                if (i === 0 && doubleFretMarkPositions.indexOf(fret) !== -1) {
+                    let doubleFretMark = tools.createElement('div');
+                    doubleFretMark.classList.add('double-fretmark');
+                    noteFret.appendChild(doubleFretMark);
+                }
+            }
+        }
+    },
+    generateNoteNames(noteIndex) {
+        noteIndex = noteIndex % 12;
+        let noteName;
+        if (accidentals === 'flats') {
+            noteName = notesFlat[noteIndex];
+        } else if (accidentals === 'sharps') {
+            noteName = notesSharp[noteIndex]
+        }
+        return noteName;
+    }
+}
+
+const tools = {
+    createElement(element, content) {
+        element = document.createElement(element);
+        if (arguments.length > 1) {
+            element.innerHTML = content;
+        }
+        return element;
+    }
+}
+
+app.init();
